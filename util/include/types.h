90804f2cada8cdad3c44207ef6404642e4501a09
mikoto2000 <mikoto2000@gmail.com>
2017-01-30 02:06:26 +0900

    Added uint types in 'types.h'.

--- util/include/types.h
+++ util/include/types.h
@@ -1,6 +1,11 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long uint64;
+
 typedef unsigned int size_t;
 
 #endif // __TIME_H__
