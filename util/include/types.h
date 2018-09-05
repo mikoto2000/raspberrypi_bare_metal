ce12889f48154dfda8df5e9ddfd755dde48db22d
mikoto2000 <mikoto2000@gmail.com>
2018-09-05 21:54:50 +0900

    `types.h` を更新。
    - uint64 にするには、 unsined long long でなければいけなかったので修正
    - size_t は標準ライブラリとバッティングするため、削除

--- util/include/types.h
+++ util/include/types.h
@@ -4,8 +4,6 @@
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
-typedef unsigned long uint64;
-
-typedef unsigned int size_t;
+typedef unsigned long long uint64;
 
 #endif // __TIME_H__
